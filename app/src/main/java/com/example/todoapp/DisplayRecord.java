a5b175798a9161838f14f71a79b402e0c6ce5808
kkitetsu <kkaku2@illinois.edu>
2019-12-09 00:36:03 -0600

    Add Picture with Conditional Decision that needs to be implemented

--- app/src/main/java/com/example/todoapp/DisplayRecord.java
+++ app/src/main/java/com/example/todoapp/DisplayRecord.java
@@ -3,6 +3,8 @@ package com.example.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DisplayRecord extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class DisplayRecord extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_record);
+
+        int record = 0;
+
+        if (record < 5) {
+            ImageView image = (ImageView)findViewById(R.id.imageGeoff);
+            image.setVisibility(View.INVISIBLE);
+        }
+
     }
 }
